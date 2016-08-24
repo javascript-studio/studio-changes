653db1ed2ecf752594a87333175262963215f5e8
Maximilian Antoni <mail@maxantoni.de>
2016-08-24 20:25:51 +0200

    Exit less aggressively in abort and improve comments

--- changes.js
+++ changes.js
@@ -9,7 +9,7 @@ const { version, author } = require('./package.json');
 const CHANGES_FILE = 'CHANGES.md';
 const CHANGES_HEADING = '# Changes\n\n';
 
-// Get previous file content:
+// Get previous file content
 let previous;
 try {
   previous = fs.readFileSync(CHANGES_FILE, 'utf8');
@@ -21,7 +21,7 @@ try {
   previous = CHANGES_HEADING;
 }
 
-// Generate changes for this release:
+// Generate changes for this release
 let changes;
 try {
   const opts = { encoding: 'utf8' };
@@ -34,22 +34,27 @@ try {
   process.exit(1);
 }
 
-// Only mention contributors:
+// Only mention contributors
 if (author) {
   const author_name = author.substring(0, author.indexOf('<')).trim();
   changes = changes.replace(new RegExp(` \\(${author_name}\\)`, 'g'), '');
 }
 
+// Generate new changes
 const remain = previous.substring(CHANGES_HEADING.length);
 const heading = `## ${version}`;
 fs.writeFileSync(CHANGES_FILE,
   `${CHANGES_HEADING}${heading}\n\n${changes}\n${remain}`);
 
+// Let the user edit the changes
 editor('CHANGES.md', (code) => {
   if (code !== 0
       || fs.readFileSync(CHANGES_FILE, 'utf8').indexOf(heading) === -1) {
+    // Abort
     fs.writeFileSync(CHANGES_FILE, previous);
-    process.exit(1);
+    process.exitCode = 1;
+  } else {
+    // Add changes file so that npm includes it in the release commit
+    execSync(`git add ${CHANGES_FILE}`);
   }
-  execSync(`git add ${CHANGES_FILE}`);
 });
