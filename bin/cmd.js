f39ccad3ac597cf7262a0cbbb180e514b861cc5b
Blade Barringer <blade@crookedneighbor.com>
2017-01-09 21:08:00 -0600

    Add help message

--- bin/cmd.js
+++ bin/cmd.js
@@ -11,9 +11,23 @@ const changes = require('..');
 
 const argv = require('minimist')(process.argv.slice(2), {
   alias: {
-    file: 'f'
+    file: 'f',
+    help: 'h'
   }
 });
+/* eslint-disable max-len */
+const HELP_TEXT = `usage: changes [--file] [--help]
+
+Options are ...
+  -f, --file [FILENAME] Specify the name of the changelog file. Defaults to CHANGES.md.
+  -h, --help            Display this help message.
+`;
+/* eslint-enable */
+
+if (argv.h || argv.help) {
+  console.log(HELP_TEXT);
+  process.exit();
+}
 
 let file = argv.file;
 
