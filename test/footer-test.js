0c1ef1832f139fd8655468811c36bb53245463db
Maximilian Antoni <mail@maxantoni.de>
2021-02-14 18:02:18 +0100

    Read user name and email from git config

--- test/footer-test.js
+++ test/footer-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const $ = require('child_process');
 const { assert, sinon } = require('@sinonjs/referee-sinon');
 const github = require('../lib/github');
 const footer = require('../lib/footer');
@@ -9,7 +10,7 @@ function today() {
 }
 
 describe('footer', () => {
-  before(() => {
+  beforeEach(() => {
     delete process.env.GIT_AUTHOR_NAME;
     delete process.env.GIT_AUTHOR_EMAIL;
   });
@@ -21,12 +22,15 @@ describe('footer', () => {
   });
 
   it('generates footer without author', async () => {
+    sinon.replace($, 'execSync', sinon.fake.throws(new Error()));
+
     const foot = await footer.generate();
 
     assert.equals(foot, `_Released on ${today()}._`);
   });
 
-  it('generates footer with author author without link', async () => {
+  it('generates footer with author from environment variable and without link', async () => {
+    sinon.replace($, 'execSync', sinon.fake.throws(new Error()));
     process.env.GIT_AUTHOR_NAME = 'Maximilian Antoni';
 
     const foot = await footer.generate();
@@ -34,7 +38,54 @@ describe('footer', () => {
     assert.equals(foot, `_Released by Maximilian Antoni on ${today()}._`);
   });
 
-  it('generates footer with author author with github homepage link', async () => {
+  it('generates footer with author from git config and without link', async () => {
+    sinon.replace(
+      $,
+      'execSync',
+      sinon.fake((cmd) => {
+        if (cmd === 'git config --get user.name') {
+          return 'Maximilian Antoni\n';
+        }
+        throw new Error();
+      })
+    );
+
+    const foot = await footer.generate();
+
+    assert.calledTwice($.execSync);
+    assert.calledWith($.execSync, 'git config --get user.name');
+    assert.calledWith($.execSync, 'git config --get user.email');
+    assert.equals(foot, `_Released by Maximilian Antoni on ${today()}._`);
+  });
+
+  it('generates footer with author from environment variable and github homepage link', async () => {
+    sinon.replace(
+      github,
+      'fetchUserHomepage',
+      sinon.fake.resolves('https://github.com/mantoni')
+    );
+    sinon.replace(
+      $,
+      'execSync',
+      sinon.fake((cmd) => {
+        if (cmd === 'git config --get user.name') {
+          return 'Maximilian Antoni\n';
+        }
+        return 'mail@maxantoni.de\n';
+      })
+    );
+
+    const foot = await footer.generate();
+
+    assert.calledOnceWith(github.fetchUserHomepage, 'mail@maxantoni.de');
+    assert.equals(
+      foot,
+      '_Released by [Maximilian Antoni](https://github.com/mantoni) on ' +
+        `${today()}._`
+    );
+  });
+
+  it('generates footer with author from git config and github homepage link', async () => {
     sinon.replace(
       github,
       'fetchUserHomepage',
